3c83df7817115be7709c40b0b212e49b95d4fc2c
LawrenceEzealor <ezealor20@gmail.com>
2023-08-28 15:22:35 +0100

    new commit

--- script.js
+++ script.js
@@ -1,22 +1,44 @@
-let firstCard = 10;
-let secondCard = 4;
-let cards = [firstCard, secondCard];// array - ordered list of items
-let sum = firstCard + secondCard 
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = "";
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+console.log(cards)
+
+function getRandomCard(){
+    
+    let randomNumber =  Math.floor( Math.random()*13 ) + 1
+    if (randomNumber > 10){
+        return 10
+    } else if (randomNumber === 1){
+        return 11
+    } else {
+        return randomNumber
+    }
+}
+
 function startGame(){
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+
     renderGame()
 }
 
+
 function renderGame() {
-    //render out firstCard and secondCard
-    cardsEl.textContent ="Cards: " + cards[0] + " " + cards[1]
-    //render out All the cards we have
+    
+    cardsEl.textContent ="Cards: "
+    for (let i = 0; i < cards.length; i++){
+        cardsEl.textContent += cards[i] + " "
+    }
+
     sumEl.textContent = "Sum: " + sum
     if ( sum <= 20){
     message = "Do you want to draw a new card?"
@@ -32,7 +54,9 @@ function renderGame() {
 }
 
 function newCard(){
-    let card = 6
+    let card = getRandomCard()
     sum += card
+    cards.push(card)
     renderGame()
 }
+
